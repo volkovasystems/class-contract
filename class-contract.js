24f67034947c6dc517644fcd0a40087864539586
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-03-02 00:06:13 +0800

    Updated class Contract.

--- class-contract.js
+++ class-contract.js
@@ -1,27 +1,50 @@
-Contract = function Contract( ){ };
+try{ var base = window }catch( error ){ var base = exports }
+( function module( base ){
+	define( "Contract",
+		function construct( ){
+			var Contract = function Contract( ){
+				var self = this;
+				this.agreeHandler = function temporaryAgreeHandler( ){ 
+					var parameters = Array.prototype.slice.apply( arguments );
+					self.agree.apply( self, parameters );
+				};
+				this.disagreeHandler = function temporaryDisagreeHandler( ){
+					var parameters = Array.prototype.slice.apply( arguments );
+					self.disagree.apply( self, parameters );
+				};
+			};
 
-Contract.prototype.agree = function agree( ){
-	var handlerTimeout;
-	var parameters = Array.prototype.slice.apply( arguments );
-	handlerTimeout = setTimeout( function handler( self ){
-		self.agreeHandler.apply( null, parameters );
-		clearTimeout( handlerTimeout );
-	}, 0, this );
-};
+			Contract.prototype.agree = function agree( ){
+				var handlerTimeout;
+				var parameters = Array.prototype.slice.apply( arguments );
+				var self = this;
+				handlerTimeout = setTimeout( function handler( ){
+					console.log( self.agreeHandler );
+					self.agreeHandler.apply( null, parameters );
+					clearTimeout( handlerTimeout );
+				}, 0 );
+			};
 
-Contract.prototype.disagree = function disagree( data ){
-	var handlerTimeout;
-	var parameters = Array.prototype.slice.apply( arguments );
-	handlerTimeout = setTimeout( function handler( self ){
-		self.disagreeHandler.apply( null, parameters );
-		clearTimeout( handlerTimeout );
-	}, 0, this );
-};
+			Contract.prototype.disagree = function disagree( data ){
+				var handlerTimeout;
+				var parameters = Array.prototype.slice.apply( arguments );
+				var self = this;
+				handlerTimeout = setTimeout( function handler( self ){
+					self.disagreeHandler.apply( null, parameters );
+					clearTimeout( handlerTimeout );
+				}, 0, this );
+			};
 
-Contract.prototype.onAgree = function onAgree( handler ){
-	this.agreeHandler = handler;
-};
+			Contract.prototype.onAgree = function onAgree( handler ){
+				this.agreeHandler = handler;
+			};
 
-Contract.prototype.onDisagree = function onDisagree( handler ){
-	this.disagreeHandler = handler;
-};
+			Contract.prototype.onDisagree = function onDisagree( handler ){
+				this.disagreeHandler = handler;
+			};
+
+			base.Contract = Contract;
+			return Contract;
+		} );
+} )( base );
+
