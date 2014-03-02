c4e4273580b0836b3a936837e7d05a4165b30aa7
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-03-03 00:11:18 +0800

    This is a failed repository. Please change the concept!

--- class-contract.js
+++ class-contract.js
@@ -1,7 +1,10 @@
-try{ var base = window }catch( error ){ var base = exports }
+try{ var base = window; }catch( error ){ var base = exports; }
 ( function module( base ){
 	define( "Contract",
-		function construct( ){
+		[
+			"async"
+		],
+		function construct( async ){
 			/*:
 				@module-documentation:
 					Contract class is an implementation of the promise
@@ -22,36 +25,66 @@ try{ var base = window }catch( error ){ var base = exports }
 						procedure for recieving the contract.
 				@end-module-documentation
 			*/
-			var Contract = function Contract( ){
+			var Contract = function Contract( namespace ){
+				//Contract.startIntervalEngine( );
+				this.namespace = namespace;
+				this.agreeHandler = this.temporaryAgreeHandler( );
+				this.disagreeHandler = this.temporaryDisagreeHandler( );
+				//Contract.addContractInterval( namespace );
+			};
+
+			/*Contract.intervalQueue = { };
+
+			Contract.addContractInterval = function addContractInterval( namespace,  ){
+				if( !( namespace in Contract.intervalQueue ) ){
+					Contract.intervalQueue[ namespace ] = 
+				}
+			};
+
+			Contract.startIntervalEngine = function startIntervalEngine( ){
+				if( Contract.hasIntervalEngineStarted ){
+					return;
+				}
+				Contract.hasIntervalEngineStarted = true;
+				Contract.intervalEngine = setInterval( function engine( ){
+					for( var contract in Contract.intervalQueue ){
+
+					}
+				}, 1 );
+			};*/
+
+			Contract.prototype.temporaryAgreeHandler = function temporaryAgreeHandler( ){
 				var self = this;
-				this.agreeHandler = function temporaryAgreeHandler( ){ 
+				return ( function temporaryHandler( ){
 					var parameters = Array.prototype.slice.apply( arguments );
 					self.agree.apply( self, parameters );
-				};
-				this.disagreeHandler = function temporaryDisagreeHandler( ){
+				} );
+			};
+
+			Contract.prototype.temporaryDisagreeHandler = function temporaryDisagreeHandler( ){
+				var self = this;
+				return ( function temporaryHandler( ){
 					var parameters = Array.prototype.slice.apply( arguments );
 					self.disagree.apply( self, parameters );
-				};
+				} );
 			};
 
 			Contract.prototype.agree = function agree( ){
-				var handlerTimeout;
 				var parameters = Array.prototype.slice.apply( arguments );
 				var self = this;
-				handlerTimeout = setTimeout( function handler( ){
-					self.agreeHandler.apply( null, parameters );
-					clearTimeout( handlerTimeout );
+				this.agreeHandlerTimeout = setTimeout( function handler( ){
+					self.agreeHandler.apply( self, parameters );
+					clearTimeout( self.agreeHandlerTimeout );
 				}, 0 );
 			};
 
 			Contract.prototype.disagree = function disagree( data ){
-				var handlerTimeout;
 				var parameters = Array.prototype.slice.apply( arguments );
 				var self = this;
-				handlerTimeout = setTimeout( function handler( self ){
-					self.disagreeHandler.apply( null, parameters );
-					clearTimeout( handlerTimeout );
-				}, 0, this );
+				this.disagreeHandlerTimeout = setTimeout( function handler( ){
+					self.disagreeHandler.apply( self, parameters );
+					clearTimeout( self.disagreeHandlerTimeout );
+				}, 0 );
 			};
 
 			Contract.prototype.onAgree = function onAgree( handler ){
@@ -66,6 +99,23 @@ try{ var base = window }catch( error ){ var base = exports }
 				return function subAssumption( ){
 					assumption( Array.prototype.slice.apply( arguments ) );
 				};
+			};
+
+			Contract.prototype.bound = function bound( contracts ){
+				if( !( contracts instanceof Array ) ){
+					var parameters = Array.prototype.slice.apply( arguments );
+					contracts = parameters;		
+				}
+
+				for( var index = 0; index < contracts.length; index++ ){
+					var contract = contracts[ index ];
+					if( !( contract instanceof Contract ) ){
+						throw new Error( "invalid contract parameter" );
+					}
+					contracts[ index ] = contract.onAgree;
+				}
+				
+				async.parallel( contracts, this.agree );
 			};
 
 			base.Contract = Contract;
